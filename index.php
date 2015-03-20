28019fb3217b22d39b62e1f645070a61917c3969
Martin Moser <moser@emineo.ch>
2015-03-20 22:38:28 +0100

    Erster Entwurf des Web-Portfolios

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head><title>TestSite</title></head>
-<body>
-    <h1>TestSite</h1>
-    <p>this repo should show running a webpage on github</p>
-    <p>PHP-Test:<?php echo 'Hello World'; ?></p>
-</body>
-</html>
