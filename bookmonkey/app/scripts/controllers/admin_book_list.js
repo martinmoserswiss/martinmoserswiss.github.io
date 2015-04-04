03751bf99ae62f203c2f2cd12dd16f43f76d8ab1
Martin Moser <moser@emineo.ch>
2015-04-04 15:14:16 +0200

    test bookmonkey

---
+++ bookmonkey/app/scripts/controllers/admin_book_list.js
@@ -0,0 +1,10 @@
+bmApp.controller('AdminBookListCtrl', function($scope, BookDataService) {
+    
+    $scope.isAdmin = true;
+
+    $scope.getBookOrder = function(book) {
+        return book.title;   
+    };
+
+    $scope.books = BookDataService.getBooks();    
+});
---
+++ bookmonkey/app/scripts/controllers/book_details.js
@@ -0,0 +1,10 @@
+bmApp.controller('BookDetailsCtrl', function ($scope, $location, $routeParams, BookDataService) {
+
+    var isbn = $routeParams.isbn;
+    
+    $scope.book = BookDataService.getBookByIsbn(isbn);
+    
+    $scope.goToListView = function () {
+        $location.path('/books');   
+    };
+});
