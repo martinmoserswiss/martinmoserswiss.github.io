45d9a862d1fa217b0f3d696069241377909632a3
Martin Moser <moser@emineo.ch>
2016-01-03 22:24:23 +0100

    correction after test

--- scripts/controllers/articles_list.js
+++ scripts/controllers/articles_list.js
@@ -1,13 +1,18 @@
 portfolioApp.controller('ArticlesCtrl', function ($scope,ArticlesDataService) {
 
     $scope.wipArticles = ArticlesDataService.getWipArticles();
-
     $scope.$watch("wipArticles | filter: filterString", function(newVal) {
-        $scope.filteredwipArticles = newVal;
+        $scope.filteredWipArticles = newVal;
+    }, true);
+    
+    $scope.dadArticles = ArticlesDataService.getDadArticles();
+    $scope.$watch("dadArticles | filter: filterString", function(newVal) {
+        $scope.filteredDadArticles = newVal;
     }, true);
 
-    /*
-    $scope.dadArticles = ArticlesDataService.getDadArticles();
     $scope.inactiveArticles = ArticlesDataService.getInactiveArticles();
-    */
+    $scope.$watch("inactiveArticles | filter: filterString", function(newVal) {
+        $scope.filteredInactiveArticles = newVal;
+    }, true);
+
 });
