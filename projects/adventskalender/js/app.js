b343bb9080fba94ee70e50007645b4bd3e0710f6
Martin Moser <mail.martinmoser@gmail.com>
2016-12-03 11:56:23 +0000

    added first content until sunday

--- projects/adventskalender/js/app.js
+++ projects/adventskalender/js/app.js
@@ -2,8 +2,43 @@ angular.module('adventskalender', ['ngRoute'])
 
 .controller('HomeCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
   $scope.avatarUrl = 'https://dummyimage.com/122x122/d59c17/fff&text='+$routeParams.day+'.12';
-  $scope.welcomeTitle = 'Herzlich Willkommen';
-  $scope.welcomeText = 'Wir freuen uns Sie auf der';
+  var day = $routeParams.day;
+  switch (day) {
+    case "1":
+        $scope.title = 'Skate or die!';
+        $scope.text = 'Einmal mit dem Skateboard um das Haus.';
+        $scope.imageUrl = 'images/skate.png'
+        $scope.link = ""
+        break;
+    case "2":
+        console.log("2");
+        $scope.title = 'The Eternal Beauty Of Snowboarding';
+        $scope.text = 'Ein geiler Snowboardfilm der du dir umbedingt reinziehen musst. Ich sage nur "What the fuck are we doing here?"';
+        $scope.imageUrl = ''
+        $scope.link = "https://www.youtube.com/watch?v=FKeUVMMl0fc"
+        break;
+    case "3":
+        $scope.title = 'Etwas gewagtes';
+        $scope.text = 'Frag öper uf dr Strass wo du chasch Schwümmflügeli choufe.';
+        $scope.imageUrl = "images/schwimmflügel.jpeg"
+        $scope.link = ""
+        break;
+    case "4":
+        $scope.title = 'Descansar (Ausruhen)';
+        $scope.text = 'Lass es dir gut gehen und nimm ein warmes Bad';
+        $scope.imageUrl = 'images/bad.png'
+        $scope.link = ""
+        break;
+    case "5":
+        $scope.title = 'Herzlich Willkommen';
+        $scope.text = 'Wir freuen uns Sie auf der';
+        break;
+    case "6":
+        $scope.title = 'Herzlich Willkommen';
+        $scope.text = 'Wir freuen uns Sie auf der';
+        break;
+    default: console.log('Not applicable...');
+  }
 }])
 
 .config(function ($routeProvider) {
