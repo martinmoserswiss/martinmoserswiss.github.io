0bd9cadaa02abaf623e386baac4c3fa31168ac04
Martin Moser <mail.martinmoser@gmail.com>
2016-12-03 10:44:48 +0000

    output

--- projects/adventskalender/js/app.js
+++ projects/adventskalender/js/app.js
@@ -3,6 +3,7 @@ angular.module('adventskalender', ['ngRoute'])
 
 .controller('HomeCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
   var day = $routeParams.day;
+  console.log(day);
 
   var avatarUrl = 'https://dummyimage.com/122x122/d59c17/fff&text='+day+'.12';
   $scope.avatarUrl = avatarUrl;
