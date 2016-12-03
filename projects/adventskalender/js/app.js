8ba395cc0906058b6b1f27b1f69aa2f4192166ee
Martin Moser <mail.martinmoser@gmail.com>
2016-12-03 10:41:01 +0000

    correction of route

--- projects/adventskalender/js/app.js
+++ projects/adventskalender/js/app.js
@@ -2,14 +2,18 @@
 angular.module('adventskalender', ['ngRoute'])
 
 .controller('HomeCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-  var day = $routeParams.param1;
-  console.log('day is: ' + day);
+  var day = $routeParams.day;
+
+  var avatarUrl = 'https://dummyimage.com/122x122/d59c17/fff&text='+day+'.12';
+  $scope.avatarUrl = avatarUrl;
+
   $scope.welcomeTitle = 'Herzlich Willkommen';
   $scope.welcomeText = 'Wir freuen uns Sie auf der';
 }])
 
 .config(function ($routeProvider) {
-    $routeProvider.when('/:param1', {
-        controller: 'HomeCtrl'
+    $routeProvider.when('/:day', {
+        controller: 'HomeCtrl',
+        templateUrl: 'templates/day.html'
     })
 });
