0d683ea63f993629af8fb1f78b640f461a40bcaf
Martin Moser <mail.martinmoser@gmail.com>
2016-12-03 10:49:52 +0000

    added dependencies

--- projects/adventskalender/js/app.js
+++ projects/adventskalender/js/app.js
@@ -1,13 +1,7 @@
-
 angular.module('adventskalender', ['ngRoute'])
 
 .controller('HomeCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-  var day = $routeParams.day;
-  console.log(day);
-
-  var avatarUrl = 'https://dummyimage.com/122x122/d59c17/fff&text='+day+'.12';
-  $scope.avatarUrl = avatarUrl;
-
+  $scope.avatarUrl = 'https://dummyimage.com/122x122/d59c17/fff&text='+$routeParams.day+'.12';
   $scope.welcomeTitle = 'Herzlich Willkommen';
   $scope.welcomeText = 'Wir freuen uns Sie auf der';
 }])
