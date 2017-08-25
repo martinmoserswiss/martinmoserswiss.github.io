c489b9149e7e161825aaa5176922079b89ba0563
Martin Moser <mail.martinmoser@gmail.com>
2017-08-25 20:04:05 +0100

    Underordered the 3 section inside a accorion.

--- scripts/controllers/articles_list.js
+++ scripts/controllers/articles_list.js
@@ -1,18 +1,23 @@
 portfolioApp.controller('ArticlesCtrl', function ($scope,ArticlesDataService) {
 
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].onclick = function(){
+            this.classList.toggle("active");
+
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
+    }
+
+    acc[0].click(); //open first panel
+
     $scope.wipArticles = ArticlesDataService.getWipArticles();
-    $scope.$watch("wipArticles | filter: filterString", function(newVal) {
-        $scope.filteredWipArticles = newVal;
-    }, true);
-
     $scope.dArticles = ArticlesDataService.getDArticles();
-    $scope.$watch("dArticles | filter: filterString", function(newVal) {
-        $scope.filteredDArticles = newVal;
-    }, true);
-
     $scope.inactiveArticles = ArticlesDataService.getInactiveArticles();
-    $scope.$watch("inactiveArticles | filter: filterString", function(newVal) {
-        $scope.filteredInactiveArticles = newVal;
-    }, true);
 
 });
