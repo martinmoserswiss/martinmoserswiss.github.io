0484ff18e74f64f12828ea6bc1def0f792a9b8c0
Martin Moser <mail.martinmoser@gmail.com>
2021-07-15 09:33:07 +0200

    image and text adjustments

--- scripts/services/articles_data.js
+++ scripts/services/articles_data.js
@@ -130,7 +130,7 @@ portfolioApp.factory('ArticlesDataService', function () {
     {
       title: 'Bronze Winner "PdG" - Mobile app',
       year: '2017-2021',
-      description: 'The PdG mobile app (Patrouille des Glaciers) manages to bring a mountain race that is difficult to access at home and makes it possible to cheer for the teams and actively motivate them. I\'m allowed to accompany this project as a project manager and requirements engineer. With this app we won the Bronze Award at the Swiss Best of Swiss App Award in the category "UX/Usability"',
+      description: 'The PdG mobile app (Patrouille des Glaciers) manages to bring a mountain race that is difficult to access at home and makes it possible to cheer for the teams and actively motivate them. I\'m allowed to accompany this project as a project manager and requirements engineer. With this app we won the Bronze Award at the Swiss Best of Swiss App Award in the category "UX/Usability".',
       tasks: [
         'Project management', 'Requirements engineering'
       ],
@@ -273,7 +273,8 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: "That SAPUI5 hybrid mobile app helps hospitals to find human resources easily and quick.",
       tasks: [
-        'SAPUI5 Java-Script- & .NET-Development'
+        'SAPUI5 with JavaScript',
+        '.NET'
       ],
       link: 'https://www.usz.ch/',
       linkText: 'https://www.usz.ch/',
@@ -293,7 +294,7 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: 'Xamarin tablet-application which enables a media disruptional maintaining of real estates.',
       tasks: [
-        'Design', '.NET-Development with Xamarin'
+        'Product Design', '.NET-Development with Xamarin'
       ],
       tags: [
         {
@@ -311,7 +312,8 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: 'This java web-application processes a huge amount of unreadable election-results into suitable graphics and tables.',
       tasks: [
-        'Java- & Java-Script-Development'
+        'Java',
+        'JavaScript'
       ],
       link: 'https://www.ag.ch',
       linkText: 'https://www.ag.ch',
@@ -331,7 +333,7 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: 'This is a native mobile app helping children with a kidney disease. The app is available for Android and iOS, where I was responsible for the iOS-version.',
       tasks: [
-        'iOS-Development with Objectiv-C'
+        'iOS with Objectiv-C'
       ],
       link: 'https://www.kispi.uzh.ch/',
       linkText: 'https://www.kispi.uzh.ch/',
@@ -351,7 +353,8 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: 'This java web-application enables the registration of graduates in the canton Aargau, Switzerland.',
       tasks: [
-        'Java- & Java-Script-Development (AJAX)'
+        'Java',
+        'JavaScript'
       ],
       link: 'https://www.ag.ch',
       linkText: 'https://www.ag.ch',
@@ -371,7 +374,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: 'Evaluation, technical take-over and development of an already existing .NET-web-application.',
       tasks: [
-        '.NET-Development (MSSQL, .NET 3.5, Windows-Services)'
+        '.NET 3.5',
+        'MSSQL',
+        'Windows-Services'
       ],
       link: 'https://www.plusdrive.ch',
       linkText: 'https://www.plusdrive.ch',
@@ -391,7 +396,11 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2014',
       description: 'Development-support in three different Java-web-applications, which are running on SAP-platforms. Electronic registration of withholding taxes, registration und authentification of third-party applications, apprenticeship portal providing information on teachers and enabling the processing of payment of contributions.',
       tasks: [
-        'Java-Development (SAP-Platform, Primefaces, FOP, SAML)'
+        'Java',
+        'SAP-Platform',
+        'Primefaces',
+        'FOP',
+        'SAML'
       ],
       link: 'https://www.services.zh.ch',
       linkText: 'https://www.services.zh.ch',
@@ -411,7 +420,12 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2013',
       description: 'A java-web-application supporting the „Real Estate Asset Management“ of Crédit Suisse. I was involved during the creation of the first version and later on while we were implementing further features like the reporting and PDF-Creation. We used open-source technologies like Spring, Hibernate, MySQL, Primefaces, Birt and some open-source libraries.',
       tasks: [
-        'Java-Development (Spring, Hibernate, MySQL, Primefaces, Birt and some open-source libraries.)'
+        'Java',
+        'Spring',
+        'Hibernate',
+        'MySQL',
+        'Primefaces',
+        'Birt'
       ],
       link: 'http://www.greenproperty.ch',
       linkText: 'http://www.greenproperty.ch',
@@ -431,7 +445,13 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2012',
       description: 'ETH Zurichs DOI Desk is the official DOI registration office for Switzerlands university and research sector. I was gathering there my first experience as a employed java-developer while I was studying. It is an open-source java web-project based on the lakshmi-framework where we were using Rich-Faces, ant, glassfish, postgres, junit, log4j and some open-source libraries.',
       tasks: [
-        'Java-Development (Rich-Faces, Ant, Glassfish, Postgres, Junit, Log4j and some open-source libraries)'
+        'Java',
+        'Rich-Faces', 
+        'Ant',
+        'Glassfish', 
+        'Postgres',
+        'Junit',
+        'Log4j'
       ],
       link: 'https://www.doi.org/',
       linkText: 'https://www.doi.org/',
@@ -451,7 +471,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2017',
       description: "Amateur is a community organization for amateur artists and young people interested in art, performance and dance. The user is able to get in contact with the organization, order items and subscribe as an artist to upcoming events. This mobile responsive web-project works with PHP, HTML and CSS. It was planed, improved and completed within 27 hours (ca. 3 work days).",
       tasks: [
-        'Planing', 'Development (HTML, CSS, PHP)'
+        'PHP',
+        'HTML',
+        'CSS'
       ],
       link: 'http://www.amateurkunst.ch',
       linkText: 'http://www.amateurkunst.ch',
@@ -471,7 +493,7 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2017',
       description: "It started as a gift for my girlfriend and again when I was in Lisbon. I had the idea of giving her a gift every day in december. Unfortunately I was not with her at that time. So sending small gifts in advance was not an option, because I was to lazy to create all 31 gifts before december. Going home everyday was ridiculous and thus not an option too. So I took it as my challenge. I came up with the idea to create a digital calendar where she was able to open every day a new gift. The gifts are coming from a remote server. Like that I had more time to prepare each gift until just before she was able to open it.",
       tasks: [
-        'Game', 'Native Android app', 'Java'
+        'Java', 'Android'
       ],
       link: 'projects/adventskalender/index.html#/13',
       linkText: 'Adventskalender',
@@ -491,7 +513,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2016',
       description: "This is a web page which shows a collection of private pictures I took.",
       tasks: [
-        'Pictures', 'Angular'
+        'JavaScript',
+        'HTML',
+        'CSS'
       ],
       link: 'projects/siClaro/index.html',
       linkText: 'siClaro',
@@ -511,7 +535,8 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2016',
       description: "A app which helps snowboarder. By proposing tricks to them, they have a tool to extend their tricks repertoire. The tricks are editable through the interface. As soon as the snowborder has some saved tricks he is able to play. In manner of the favorite Game of S.N.O.W he is then able to play with a friend.",
       tasks: [
-        'Game', 'Native iOS app', 'Swift'
+        'iOS',
+        'Swift'
       ],
       tags: [
         {
@@ -529,7 +554,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2016',
       description: "When I moved to Lisbon in 2016 I decided to create a own version of a tinder app for my girlfriend. The app works more or less like a incomplete Tinder version. The set of swipeable cards contains portraits of me but also from other random guys. Swiping cards left or right is just allowed depending on the underlying image.",
       tasks: [
-        'Game', 'Hybrid mobile app', 'Ionic (HTML, CSS, Java-Script)', 'Target: Android'
+        'Hybrid mobile app', 
+        'Ionic (HTML, CSS, Java-Script)',
+        'Target: Android'
       ],
       tags: [
         {
@@ -547,7 +574,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: "Is a film-festival I organized with a friend and my brother. The idea behind the event, was to show all the emotions and impressions we gathered on our trips. For the first edition of our FFF we brought together six movies from six people who travelled to/around four different countries.",
       tasks: [
-        'Planing', 'Development (HTML, CSS, Java-Script)'
+        'JavaScript',
+        'HTML',
+        'CSS'
       ],
       link: 'http://ferienfilme.github.io/',
       linkText: 'http://ferienfilme.github.io/',
@@ -567,7 +596,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2015',
       description: "This was an IoT-Project I was doing with my brother. The product would help people to take care of their plants. We were programming with C++ on an Arduino-board. Pumping water through pipes and relays. For the other part of the software we were using Angular, Java and iOS as frontend technologies. The backend worked with MQTT, Maven, Spring, Jersey, Hibernate and MySQL.",
       tasks: [
-        'Concept', 'Java/Java-Script-Development'
+        'Concept',
+        'Java',
+        'JavaScript'
       ],
       tags: [
         {
@@ -585,7 +616,10 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2014',
       description: "Is my aunt and uncle’s traditional family restaurant based in the German part of the Jura in Solothurn, Switzerland. This website was made with wordpress and is now completely maintained by the owners. I organized the production of all pictures, which were taken by a friend.",
       tasks: [
-        'Planing', 'Development (Wordpress)'
+        'PHP',
+        'HTML',
+        'CSS',
+        'Wordpress'
       ],
       link: 'http://www.hinterweissenstein.ch',
       linkText: 'http://www.hinterweissenstein.ch',
@@ -605,7 +639,10 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2014',
       description: "This page was for a dental hygiene office in Bern, Switzerland. The concept of that e-card-page had a high profile in the web.",
       tasks: [
-        'Planing', 'Development (HTML, CSS, Java-Script)', 'Maintaining'
+        'PHP',
+        'HTML',
+        'CSS',
+        'Wordpress'
       ],
       tags: [
         {
@@ -623,7 +660,10 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2014',
       description: 'A registration- and information page for a neurology-congress in Berne.',
       tasks: [
-        'Planing', 'Development (Wordpress)'
+        'PHP',
+        'HTML',
+        'CSS',
+        'Wordpress'
       ],
       tags: [
         {
@@ -641,7 +681,13 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2013',
       description: 'Bachelor thesis in the context of education for BSc in computer science. The Neglect Analysis software allows automated analysis of patient tests, which were carried out with graphic software. The results are presented to the user in the form of charts and serve the diagnosis. Planning and implementation of Java productively deployed application.',
       tasks: [
-        'Planing', 'Java-Development (Swing, Derby, Log4j, PDFJet, JFreeChart)'
+        'Concept', 
+        'Java',
+        'Swing', 
+        'Derby', 
+        'Log4j', 
+        'PDFJet', 
+        'JFreeChart'
       ],
       link: 'https://bfh.easydocmaker.ch/media/pdf_final/357_Martin_Moser.pdf',
       linkText: 'Paper',
@@ -661,7 +707,10 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2013',
       description: 'An information page covering the topic of transcranial magnetic stimulation in therapy.',
       tasks: [
-        'Planing', 'Development (Wordpress)'
+        'PHP',
+        'HTML',
+        'CSS',
+        'Wordpress'
       ],
       link: 'http://www.tms-therapie.ch',
       linkText: 'http://www.tms-therapie.ch',
@@ -681,7 +730,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2010',
       description: 'This was a Portrait of the construction-company in Limpach, Switzerland. At that time I created that web-page I was working for that company as a construction worker too. The work was hard but I appreciate that time as a good experience in my life. I enjoyed the photo-project where me and a friend took portrait-pictures of all 35 employees. Finally we integrated them on the web-page which was the project-completion and afterwards the time with the people too. The web-page was maintainable by a tiny CMS which enables the user to edit the employees and news-posts. I used PHP and Java-Script/AJAX as programming languages.',
       tasks: [
-        'Planing', 'PHP- and Java-Script-Development'
+        'PHP',
+        'HTML',
+        'CSS',
       ],
       tags: [
         {
@@ -699,7 +750,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2010',
       description: 'This project was an election-page for Verena Moser when she ran for the Swiss national council. The page contained a tiny CMS to edit news posts and was written in PHP and Java-Script.',
       tasks: [
-        'Planing', 'PHP- and Java-Script-Development'
+        'PHP',
+        'HTML',
+        'CSS'
       ],
       tags: [
         {
@@ -717,7 +770,9 @@ portfolioApp.factory('ArticlesDataService', function () {
       year: '2010',
       description: 'This was an administration-web-application for a private massage salon. The user recorded and edited patients and charged them the expenses. During and after a treatment period he had the opportunity to issue a bill to the patient or directly to the insurance. An automated PDF generator did that job. To be reminded of birthdays of customers, he could enable the feature „Birthday-Reminder“. The application was running on a web server, which was operated by a notebook. I used PHP and Java-Script/AJAX as programming languages.',
       tasks: [
-        'Planing', 'PHP- and Java-Script-Development'
+        'PHP',
+        'HTML',
+        'CSS',
       ],
       tags: [
         {
